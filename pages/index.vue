7ae8c232c7ba76c1975f07ed380f508158468ebf
FullZero <fullzero5@gmail.com>
2018-08-30 10:46:01 +0000

    add style

--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,18 @@ export default {
   components: {
     AppHero: Hero
   },
-  computed: {
-    
-  },
-  created () {
-    
-  }
+  computed: { },
+  created () { }
 }
 </script>
+
+<style lang="stylus" scoped>
+  .content
+    /*no grid support*/
+    width 79.7872%
+    /* grid */
+    display grid
+    grid-template-columns repeat(3, 1fr)
+    grid-gap 1rem
+    padding 0
+</style>
