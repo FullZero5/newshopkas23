bf881537678da2c3860752cd044c57a8f8ddc4dc
FullZero <fullzero5@gmail.com>
2018-08-30 13:34:06 +0000

    use product vuex

--- store/index.js
+++ store/index.js
@@ -1,12 +1,14 @@
 import Vuex from 'vuex'
 import { firebaseMutations } from 'vuexfire'
 import cart from '@/store/modules/cart'
+import product from '@/store/modules/product'
 
 const store = () => {
   return new Vuex.Store({
     strict: process.env.NODE_ENV !== 'production',
     modules: {
-      cart
+      cart,
+      product
     },
     mutations: {
       ...firebaseMutations
