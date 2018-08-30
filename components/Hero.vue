c11350829fbcef0efe5145d29c55e60b3501602c
FullZero <fullzero5@gmail.com>
2018-08-30 09:51:45 +0000

    add small hero layer

---
+++ components/Hero.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+  .container
+    p.title <strong class="has-text-info">Онлайн кассы под ФЗ-54,</strong> в Краснодаре
+    p.subtitle Фискальные регистраторы и автономные аппарты, производства: Штрих, Атол, ЭКР, Инкотекс.<br>Услуги по вводу в эксплуатацию и регистрации в ИФНС <strong class="has-text-info">гарантийный ремонт</strong>.
+</template>
