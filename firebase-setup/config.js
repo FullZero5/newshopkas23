7931582a33b4226940b53c9eb2d83c6c0cb2b5e4
FullZero <fullzero5@gmail.com>
2018-08-30 10:42:09 +0000

    add config firebase

---
+++ firebase-setup/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyARYZku4kgfdWI4uggJf86uSk9WPHdIlkw',
+  authDomain: 'kas23-757ee.firebaseapp.com',
+  databaseURL: 'https://kas23-757ee.firebaseio.com',
+  projectId: 'kas23-757ee',
+  storageBucket: 'kas23-757ee.appspot.com',
+  messagingSenderId: '498268488236'
+}
