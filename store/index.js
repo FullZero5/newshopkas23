21cb2a53dc8d78381752174e60d7761d7172588f
FullZero <fullzero5@gmail.com>
2018-08-30 10:47:20 +0000

    add firebase mutation

--- store/index.js
+++ store/index.js
@@ -1,14 +1,15 @@
 import Vuex from 'vuex'
+import { firebaseMutations } from 'vuexfire'
 import cart from '@/store/modules/cart'
 
 const store = () => {
   return new Vuex.Store({
     strict: process.env.NODE_ENV !== 'production',
     modules: {
-     cart
+      cart
     },
     mutations: {
-      
+      ...firebaseMutations
     }
   })
 }
